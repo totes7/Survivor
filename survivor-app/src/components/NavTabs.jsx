3f9e8a0ba7c4d57df9afd4e1b53a13eec2ab11a3
Antonio Manno <antonio.manno.7@gmail.com>
2024-07-01 12:48:51 +0100

    first navbar setup

---
+++ survivor-app/src/components/NavTabs.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavTabs.css";
+
+function NavTabs() {
+    return (
+        <>
+      <nav className="navbar navbar-expand-md">
+        <div className="container-fluid">
+          <NavLink to="/" end className="logo">
+            SURVIVOR
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link">
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link">
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/"
+                  end
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="about"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="contact"
+                  end
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+    );
+}
+
+export default NavTabs;
