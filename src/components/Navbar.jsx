b9c541ff3c9f676b190b1caabed076a08410f1c2
Antonio Manno <antonio.manno.7@gmail.com>
2024-11-22 10:54:49 +0000

    modify navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,79 +1,70 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import Dropdown from "./Dropdown";
-import logo from "../images/LogoBBG.png";
+// import Dropdown from "./Dropdown";
+// import logo from "../images/LogoBBG.png";
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [dropdown, setDropdown] = useState(false);
+  // const [dropdown, setDropdown] = useState(false);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
-  const onMouseEnter = () => {
-    if (window.innerWidth < 960) {
-      setDropdown(false);
-    } else {
-      setDropdown(true);
-    }
-  };
+  // const onMouseEnter = () => {
+  //   if (window.innerWidth < 960) {
+  //     setDropdown(false);
+  //   } else {
+  //     setDropdown(true);
+  //   }
+  // };
 
-  const onMouseLeave = () => {
-    if (window.innerWidth < 960) {
-      setDropdown(false);
-    } else {
-      setDropdown(false);
-    }
-  };
+  // const onMouseLeave = () => {
+  //   if (window.innerWidth < 960) {
+  //     setDropdown(false);
+  //   } else {
+  //     setDropdown(false);
+  //   }
+  // };
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <div className="navbar-logo">
+          {/* <div className="navbar-logo">
             <Link to="/" onClick={closeMobileMenu}>
               <img src={logo} alt="survivor logo" />
             </Link>
-          </div>
+          </div> */}
 
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
+            
             <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 HOME
               </Link>
             </li>
-            
-            <li>
+            <li className="nav-item">
               <Link
                 to="/contact"
-                className="nav-links"
+                className="nav-links mobile-link"
                 onClick={closeMobileMenu}
               >
                 CONTACT
               </Link>
             </li>
-            <li
+            {/* <li
               className="nav-item dropdown-button"
               onMouseEnter={onMouseEnter}
               onMouseLeave={onMouseLeave}
             >
-                MORE &nbsp; <i className="fas fa-caret-down" />
-              {dropdown && <Dropdown />}
-            </li>
-            <li>
-              <Link
-                to="/about"
-                className="nav-links mobile-link"
-                onClick={closeMobileMenu}
-              >
-                ABOUT
-              </Link>
-            </li>
-            <li>
+            MORE &nbsp; <i className="fas fa-caret-down" />
+            {dropdown && <Dropdown />}
+            </li> */}
+            <li className="nav-item">
               <Link
                 to="/info"
                 className="nav-links mobile-link"
@@ -82,7 +73,16 @@ function Navbar() {
                 PRODUCTION
               </Link>
             </li>
-            <li>
+            <li className="nav-item">
+              <Link
+                to="/about"
+                className="nav-links mobile-link"
+                onClick={closeMobileMenu}
+              >
+                ABOUT
+              </Link>
+            </li>
+            <li className="nav-item">
               <Link
                 to="/media"
                 className="nav-links mobile-link"
@@ -91,16 +91,16 @@ function Navbar() {
                 MEDIA
               </Link>
             </li>
-            <li>
+            <li className="nav-item">
               <Link
                 to="/screening"
                 className="nav-links mobile-link"
                 onClick={closeMobileMenu}
               >
-                SCREENINGS
+                EVENTS
               </Link>
             </li>
-            <li>
+            <li className="nav-item">
               <Link
                 to="/book"
                 className="nav-links mobile-link"
