fdd778571852cfe33318ec89c4e58af96bb74973
Antonio Manno <antonio.manno.7@gmail.com>
2024-12-09 14:43:44 +0000

    install react-lazy-load for images in gallery

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../App.css";
 import Footer from "../Footer";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function Gallery() {
   const images = require.context("../../images/gallery", true);
@@ -12,7 +13,13 @@ function Gallery() {
       <div className="gallery">
         <h1>GALLERY</h1>
         {imagesList.map((image, index) => (
-            <img key={index} src={image} alt={`image-${index}`} />
+          <LazyLoadImage
+            key={index}
+            src={image}
+            alt={`image-${index}`}
+            effect="blur"
+            className="gallery-img"
+          />
         ))}
       </div>
       <Footer />
