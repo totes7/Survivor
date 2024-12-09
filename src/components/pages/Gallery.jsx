a68b42f56002e19f91aa8999a1bb807c39167292
Antonio Manno <antonio.manno.7@gmail.com>
2024-12-09 16:01:28 +0000

    uninstall lazy image component library

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../../App.css";
 import Footer from "../Footer";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function Gallery() {
   const images = require.context("../../images/gallery", true);
@@ -13,12 +12,13 @@ function Gallery() {
       <div className="gallery">
         <h1>GALLERY</h1>
         {imagesList.map((image, index) => (
-          <LazyLoadImage
+          <img
             key={index}
             src={image}
             alt={`image-${index}`}
             effect="blur"
             className="gallery-img"
+            loading="lazy"
           />
         ))}
       </div>
