526b0f0e4bd65e2b8a69eeb6b309b76b61126171
Antonio Manno <antonio.manno.7@gmail.com>
2024-12-09 14:17:07 +0000

    write function to display all images in gallery page

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -3,9 +3,18 @@ import "../../App.css";
 import Footer from "../Footer";
 
 function Gallery() {
+  const images = require.context("../../images/gallery", true);
+
+  const imagesList = images.keys().map((image) => images(image));
+
   return (
     <>
-      <div className="gallery"></div>
+      <div className="gallery">
+        <h1>GALLERY</h1>
+        {imagesList.map((image, index) => (
+            <img key={index} src={image} alt={`image-${index}`} />
+        ))}
+      </div>
       <Footer />
     </>
   );
