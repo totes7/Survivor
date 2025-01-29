395d547a6e5df52ada6d2e86e6b99661f39734a4
Antonio Manno <antonio.manno.7@gmail.com>
2025-01-29 10:54:57 +0000

    fix events page and add link to jw3 cinema showtimes

--- src/components/pages/Screening.jsx
+++ src/components/pages/Screening.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../../App.css";
 import Footer from "../Footer";
 // import banner from "../../images/Poster.jpg";
 
 function Screening() {
-  
-
   return (
     <>
       <div className="screening">
@@ -14,12 +12,31 @@ function Screening() {
           <h1>EVENTS</h1>
         </div>
 
-        <p>New Events coming soon!</p>
+        <p>Upcoming events to be announced shortly</p>
+        <br />
+        <br />
+
+
+        <Link to="https://www.jw3.org.uk/search?for=Survivor" target="_blank" className="cinema-link">
+          <em>Survivor</em> cinema showtimes
+        </Link>
+
         <br />
         <br />
         <br />
         <br />
         <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+
 
         {/* <p>
           Special Screening with JW3, London, to mark Holocaust Memorial Day 2025
@@ -35,7 +52,6 @@ function Screening() {
         >
           <button>BOOK</button>
         </Link> */}
-
       </div>
 
       <Footer />
